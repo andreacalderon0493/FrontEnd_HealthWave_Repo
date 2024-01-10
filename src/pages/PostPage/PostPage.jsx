12ea9716532ed4133d5c1075f5092445f73f8c97
Andrea Calderon <andreacalderon@MacBook-Pro-2.local>
2024-01-10 17:38:33 -0500

    added PostPage component

---
+++ src/pages/PostPage/PostPage.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const PostPage = ({}) => {
+  return (
+    <div>
+      <h1>Post Page</h1>
+    </div>
+  );
+};
+
+export default PostPage;
